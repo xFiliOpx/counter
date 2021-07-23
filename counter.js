618cea56dbae360136cf86f44270f4b3899ff901
xFiliOpx <smietanka2004@gmail.com>
2021-07-23 23:55:59 +0200

    first commit

---
+++ counter.js
@@ -0,0 +1,15 @@
+let p1Name = "Player 1";
+let p2Name = "Player 2";
+
+function check(name){
+    if(name == "p1Name"){
+        document.write(p1Name);
+    }
+    else{
+        document.write(p2Name);
+    }
+}
+
+function displayAlert(){
+    alert("alerteryrye");
+}
